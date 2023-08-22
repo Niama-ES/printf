15d9215c4a70bb803e5f57e748821da976ed445c
Niama-ES <developerzone91@gmail.com>
2023-08-22 08:50:56 -0700

    printf

--- specifier.c
+++ specifier.c
@@ -62,7 +62,9 @@ return (4);
 func_print_rev(lv, para);
 return (0);
 }
-int (*f)(va_list, paras *) = get_specifier(s);
+int (*f)(va_list, paras *);
+
+(*f)(va_list, paras *) = get_specifier(s);
 if (f)
 return (f(lv, para));
 return (0);
